ddbe10387ffd9714d1afa0df6944ab2ac96dd8bb
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2017-04-19 21:55:51 -0400

    Add 'setup.py' file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+VERSION = 1.0
+CAH_VERSION = 1.3
+
+setup(name='python-discord-cah',
+      version=VERSION ,
+      description='A Cards Against Humanity discord bot',
+      author='Lucien Gaitskell',
+      url='https://github.com/luciengaitskell',
+      install_requires=['python-cah==' + str(CAH_VERSION), 'discord.py'],
+      dependency_links=[('http://github.com/luciengaitskell/python-cah/tarball/master#egg=python-cah-'
+                         + str(CAH_VERSION))],
+      packages=find_packages()
+      )
