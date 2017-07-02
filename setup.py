3ef5cba4b4a05800295ff74dbd0c31fc3cf143d2
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2017-07-02 10:24:11 -0400

    Prefix 'python-cah' version with 'v' in setup script

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup, find_packages
 
 VERSION = "v1.7"
-CAH_VERSION = 1.4
+CAH_VERSION = "v1.4"
 
 setup(name='python-discord-cah',
       version=VERSION ,
       description='A Cards Against Humanity discord bot',
       author='Lucien Gaitskell',
       url='https://github.com/luciengaitskell',
-      install_requires=['python-cah==' + str(CAH_VERSION), 'discord.py'],
-      dependency_links=[('http://github.com/luciengaitskell/python-cah/tarball/master#egg=python-cah-'
-                         + str(CAH_VERSION))],
+      install_requires=['python-cah==' + CAH_VERSION, 'discord.py'],
+      dependency_links=[('http://github.com/luciengaitskell/python-cah/tarball/master#egg=python-cah-' + CAH_VERSION)],
       packages=find_packages()
       )
